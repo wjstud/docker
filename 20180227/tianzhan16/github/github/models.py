edb938544195729c3c5e4d99549cc297752e4f2a
wjepower <wjepower@hotmail.com>
2018-02-27 17:57:32 +0800

    tiaozhan16 OK

---
+++ 20180227/tianzhan16/github/github/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine, Column, String, Integer, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('mysql+mysqldb://root@localhost:3306/shiyanlougithub?charset=utf8')
+Base = declarative_base()
+
+class Repository(Base):
+    __tablename__ = 'repositories'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64))
+    update_time = Column(DateTime)
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
---
+++ 20180227/tianzhan16/github/github/pipelines.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from sqlalchemy.orm import sessionmaker
+from github.models import Repository, engine
+
+class GithubPipeline(object):
+    def process_item(self, item, spider):
+        self.session.add(Repository(**item))
+        return item
+
+    def open_spider(self, spider):
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
+
+    def close_spider(self, spider):
+        self.session.commit()
+        self.session.close()
