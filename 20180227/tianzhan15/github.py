2977b388948ceaacc76c754df8fe88a409c3c633
wjepower <wjepower@hotmail.com>
2018-02-27 14:16:22 +0800

    tianzhan15 Ok

---
+++ 20180227/tianzhan15/github.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+class GthbSpider(scrapy.Spider):
+    name = 'gthb'
+#    allowed_domains = ['github.com']
+
+    def start_requests(self):
+        url_tmpl = 'https://github.com/shiyanlou?tab=repositories&page={}'
+        urls = (url_tmpl.format(i) for i in range(1,4))
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        for rep in response.xpath('//li[@class="col-12 d-block width-full py-4 border-bottom public source"]'):
+         #   item = GithubItem()
+            yield {
+                    'name': rep.xpath('.//a/text()').extract_first().strip(),
+                    'update_time': rep.xpath('.//relative-time/@datetime').extract_first().strip()
+                    }
