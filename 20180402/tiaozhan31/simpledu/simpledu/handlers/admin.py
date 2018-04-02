ac54a4a531531511f6e6a8f98c7c71b607d97f2a
wjstud <wjepower@hotmail.com>
2018-04-02 21:40:41 +0800

    tiaozhan31 ok

---
+++ 20180402/tiaozhan31/simpledu/simpledu/handlers/admin.py
@@ -0,0 +1,93 @@
+from flask import Blueprint, render_template, request, current_app, redirect, url_for, flash
+from simpledu.decorators import admin_required
+from simpledu.models import db, Course, User
+from simpledu.forms import CourseForm, UserForm
+
+admin = Blueprint('admin', __name__, url_prefix='/admin')
+
+@admin.route('/')
+@admin_required
+def index():
+    return render_template('admin/index.html')
+
+@admin.route('/users')
+@admin_required
+def users():
+    page = request.args.get('page', default=1, type=int)
+    pagination = User.query.paginate(
+            page=page,
+            per_page=current_app.config['ADMIN_PER_PAGE'],
+            error_out=False
+            )
+    return render_template('admin/users.html', pagination=pagination)
+
+@admin.route('/users/create', methods=['GET', 'POST'])
+@admin_required
+def create_user():
+    form = UserForm()
+    if form.validate_on_submit():
+        form.create_user()
+        flash('user add success', 'success')
+        return redirect(url_for('admin.users'))
+    return render_template('admin/create_user.html', form=form)
+
+@admin.route('/users/<int:user_id>/edit', methods=['GET', 'POST'])
+@admin_required
+def edit_user(user_id):
+    user = User.query.get_or_404(user_id)
+    form = UserForm(obj=user)
+    if form.validate_on_submit():
+        form.update_user(user)
+        flash('user update success', 'success')
+        return redirect(url_for('admin.users'))
+    return render_template('admin/edit_user.html', form=form, user=user)
+
+@admin.route('/courses/<int:user_id>/delete')
+@admin_required
+def delete_user(user_id):
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    flash('user remove success', 'success')
+    return redirect(url_for('admin.users'))
+
+@admin.route('/courses')
+@admin_required
+def courses():
+    page = request.args.get('page', default=1, type=int)
+    pagination = Course.query.paginate(
+            page=page,
+            per_page=current_app.config['ADMIN_PER_PAGE'],
+            error_out=False
+            )
+    return render_template('admin/courses.html', pagination=pagination)
+
+@admin.route('/courses/create', methods=['GET', 'POST'])
+@admin_required
+def create_course():
+    form = CourseForm()
+    if form.validate_on_submit():
+        form.create_course()
+        flash('课程创建成功', 'success')
+        return redirect(url_for('admin.courses'))
+    return render_template('admin/create_course.html', form=form)
+
+@admin.route('/courses/<int:course_id>/edit', methods=['GET', 'POST'])
+@admin_required
+def edit_course(course_id):
+    course = Course.query.get_or_404(course_id)
+    form = CourseForm(obj=course)
+    if form.validate_on_submit():
+        form.update_course(course)
+        flash('course update success', 'success')
+        return redirect(url_for('admin.courses'))
+    return render_template('admin/edit_course.html', form=form, course=course)
+
+@admin.route('courses/<int:course_id>/delete')
+@admin_required
+def delete_course(course_id):
+    course = Course.query.get_or_404(course_id)
+    db.session.delete(course)
+    db.session.commit()
+    flash('course remove success', 'success')
+    return redirect(url_for('admin.courses'))
